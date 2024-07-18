b91dfb35edbe30214051596e41842b15a0019eac
Charlie Kim <charlie2@ualberta.ca>
2024-07-17 21:06:42 -0600

    part4 init

--- part2/phonebook/src/service/db.js
+++ part2/phonebook/src/service/db.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const personsUrl = (id) => `http://localhost:3001/persons/${id}`;
+const personsUrl = (id) => `http://localhost:3001/api/persons/${id}`;
 
 const create = (person) => {
   return axios.post(personsUrl(""), person);
